fd912436d59f593df8600d167095e29227ed2ff5
Wasiq Naqi <wasiq.naqi@invozone.com>
2022-07-26 10:53:12 +0500

    Update blank page issue

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ const config = {
         new CopyWebpackPlugin({
             patterns: [
               { from: "assets", to: "assets" },
-              { from: "index.html", to: "" },
+              { from: "src/index.html", to: "" },
             ],
         })
 	]
